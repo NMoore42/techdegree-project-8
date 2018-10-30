c70620ab0a8ff2d60a04ac694f520583627cd822
Nick Moore <nicholas.moore5@icloud.com>
2018-10-30 12:38:57 -0400

    Added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,48 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
     sass = require('gulp-sass'),
-    maps = require('gulp-sourcemaps');
+    maps = require('gulp-sourcemaps'),
+   bSync = require('browser-sync').create();
 
 gulp.task('concatScripts', function () {
-  gulp.src([
+  return gulp.src([
     'js/circle/autogrow.js',
     'js/circle/circle.js',
     'js/global.js'])
   .pipe(concat('app.js'))
-  .pipe(gulp.dest('js')); 
+  .pipe(gulp.dest('js'));
 });
 
-gulp.task('minifyScripts', function () {
-  gulp.src('js/app.js')
+gulp.task('minifyScripts', ['concatScripts'], function () {
+  return gulp.src('js/app.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('js'));
 });
 
 gulp.task('compileSass', function () {
-  gulp.src('sass/global.scss')
+  return gulp.src('sass/global.scss')
     .pipe(maps.init())
     .pipe(sass())
-    .pipe(write('./'))
-    .pipe(gulp.dest('css'));
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('css'))
+    .pipe(bSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('default', ['hello']);
+gulp.task('watchSass', ['browserSync', 'compileSass'], function () {
+  return gulp.watch('sass/*.scss', ['compileSass']);
+});
+
+gulp.task('browserSync', function () {
+  bSync.init({
+    server: {
+      baseDir: './'
+    },
+  })
+});
+
+gulp.task('build', ['minifyScripts', 'compileSass']);
+
+gulp.task('default', ['build', 'watchSass']);
